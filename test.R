21bcd4be3512f77c0ac4c982d9495a706c0a1ca9
Wes Stone <wesstone@microsoft.com>
2024-06-19 17:57:42 -0700

    testing yolo

---
+++ test.R
@@ -0,0 +1,47 @@
+library(devtools)
+
+#load the package
+load_all()
+
+# \donttest{
+data_tbl <- timetk::m4_monthly %>%
+  dplyr::rename(Date = date) %>%
+  dplyr::mutate(id = as.character(id)) %>%
+  dplyr::filter(
+    id == "M2",
+    Date >= "2012-01-01",
+    Date <= "2015-06-01"
+  )
+
+run_info <- set_run_info()
+
+prep_data(run_info,
+  input_data = data_tbl,
+  combo_variables = c("id"),
+  target_variable = "value",
+  date_type = "month",
+  forecast_horizon = 3,
+  recipes_to_run = "R1"
+)
+
+prep_models(run_info,
+  models_to_run = c("meanf"),
+  num_hyperparameters = 1
+)
+
+
+train_models(run_info,
+  run_local_models = TRUE
+)
+
+
+
+final_models(run_info,
+  average_models = FALSE
+)
+
+fcst_tbl <- get_forecast_data(run_info)
+# }
+
+View(fcst_tbl)
+print(fcst_tbl)
